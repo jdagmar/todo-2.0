eda01f43c4303e1c0ce7b778ff0ad88ca9ee96ca
jdagmar <dagmar.j.gustavsson@gmail.com>
2018-02-14 12:57:12 +0100

    minor style changes

--- main.js
+++ main.js
@@ -10,7 +10,31 @@ const deleteButton = document.getElementById('delete-button');
 const todoEl = '';
 const doneEl = '';
 
-const addTodo = (todoTitle) => {
+let todos = [
+    { title: 'This is a finished task', completed: true },
+    { title: 'This is an unfinished task', completed: false }
+];
+
+const isValidInput = (todoTitle) => {
+    const alert = document.getElementById('alert');
+
+    for (todo of todos) {
+        if (todo.title === todoTitle) {
+            alert.classList.remove('hidden');
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+const addTodo = (todoTitle, validate) => {
+
+    if (validate && !isValidInput(todoTitle)){
+       return;
+    }
+
     const todoEl = template.cloneNode(true);
     todoEl.id = '';
 
@@ -33,16 +57,16 @@ const addTodo = (todoTitle) => {
 
 const saveTodo = () => {
     const todoItems = todoList.querySelectorAll('.todo-item:not([id="list-item-template"]) .todo-title');
-    const todos = [];
+    todos = [];
 
-    for (const todo of todoItems){
-        todos.push({title: todo.innerText, completed: false});
+    for (const todo of todoItems) {
+        todos.push({ title: todo.innerText, completed: false });
     }
 
     const completedItems = completedList.querySelectorAll('.todo-item:not([id="done-list-item-template"]) .todo-title');
 
-    for (const todo of completedItems){
-        todos.push({title: todo.innerText, completed: true});
+    for (const todo of completedItems) {
+        todos.push({ title: todo.innerText, completed: true });
     }
 
     window.localStorage.todoData = JSON.stringify(todos);
@@ -69,7 +93,9 @@ const deleteTodo = (todoEl) => {
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-    addTodo(input.value);
+    addTodo(input.value, true);
+    input.value = '';
+    input.focus();
 });
 
 const deleteAll = (list) => {
@@ -88,17 +114,12 @@ const deleteAllCompletedButton = document.getElementById('delete-all-completed')
 deleteAllTodosButton.addEventListener('click', () => deleteAll(todoList));
 deleteAllCompletedButton.addEventListener('click', () => deleteAll(completedList));
 
-let todos = [
-    { title: 'This is a finished task', completed: true },
-    { title: 'This is an unfinished task', completed: false }
-];
-
 if (window.localStorage.todoData) {
     todos = JSON.parse(window.localStorage.todoData);
 }
 
 for (const todo of todos) {
-    const todoEl = addTodo(todo.title);
+    const todoEl = addTodo(todo.title, false);
 
     if (todo.completed) {
         checkOff(todoEl);
